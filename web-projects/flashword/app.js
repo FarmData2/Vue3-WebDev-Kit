2561f7d8c1bda6b9e9cda91c7559090923e8dfae
Grant Braught <braught@dickinson.edu>
2026-07-24 19:46:58 +0000

    removes old code and setups blank template to build the app

--- web-projects/flashword/app.js
+++ web-projects/flashword/app.js
@@ -1,84 +1,10 @@
 const Flashword = {
   data() {
-    return {
-      wordA: 'hola',
-      wordB: 'hello',
-      answer: '',
-      correct: null,
-      showFeedback: false,
-      image: null,
-      imageAlt: null,
-      hasError: false,
-      inputBackgroundColor: 'white',
-      showHint: false,
-      level: 'easy',
-      sentence: '',
-      lastName: '',
-      firstName: '',
-      firstNameAndLastName: '',
-
-      // Array example
-      spanishWords: ['hola', 'adios', 'uno', 'dos'],
-
-      // Object example
-      word: { a: 'hola', b: 'hello' },
-
-      // Array of objects example
-      words: [
-        { wordA: 'hola', wordB: 'hello' },
-        { wordA: 'adios', wordB: 'goodbye' },
-        { wordA: 'uno', wordB: 'one' },
-        { wordA: 'dos', wordB: 'two' },
-      ],
-    }
+    return {}
   },
-  computed: {
-    fullName() {
-      return this.firstName + ' ' + this.lastName
-    },
-    shortSpanishWords() {
-      return this.spanishWords.filter((word) => word.length <= 3)
-    },
-  },
-  watch: {
-    firstName() {
-      this.firstNameAndLastName = this.firstName + ' ' + this.lastName
-    },
-    lastName() {
-      this.firstNameAndLastName = this.firstName + ' ' + this.lastName
-    },
-  },
-  methods: {
-    getFullName() {
-      return this.firstName + ' ' + this.lastName
-    },
-    checkAnswer() {
-      if (this.answer == '') {
-        this.hasError = true
-        this.inputBackgroundColor = 'lightpink'
-        return
-      }
-
-      this.hasError = false
-      this.inputBackgroundColor = 'white'
-
-      this.correct = this.wordB == this.answer
-
-      if (this.correct) {
-        this.image = 'correct'
-        this.imageAlt = 'Green check mark.'
-      } else {
-        this.image = 'incorrect'
-        this.imageAlt = 'Red X.'
-      }
-
-      this.showFeedback = true
-    },
-    reset() {
-      this.answer = ''
-      this.showFeedback = false
-    },
-  },
+  computed: {},
+  watch: {},
+  methods: {},
 }
 
 // eslint-disable-next-line no-unused-vars, no-undef
