1d1996c1136003ba6e134f77e57465f562caa339
Grant Braught <braught@dickinson.edu>
2026-07-25 13:31:59 +0000

    adds words data property

--- web-projects/flashword/app.js
+++ web-projects/flashword/app.js
@@ -1,6 +1,30 @@
 const Flashword = {
   data() {
-    return {}
+    return {
+      words: [
+        {
+          word_a: 'hola',
+          word_b: 'hello',
+          hint: 'greeting',
+          answer: '',
+          correct: false,
+        },
+        {
+          word_a: 'uno',
+          word_b: 'one',
+          hint: 'number',
+          answer: '',
+          correct: false,
+        },
+        {
+          word_a: 'gris',
+          word_b: 'grey',
+          hint: 'color',
+          answer: '',
+          correct: false,
+        },
+      ],
+    }
   },
   computed: {},
   watch: {},
