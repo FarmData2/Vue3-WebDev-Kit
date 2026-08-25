68a342491c4468909f749e5a9e4f7495750b2b17
Grant Braught <braught@dickinson.edu>
2026-08-25 12:21:28 +0000

    updates header text to match in all cases

--- web-projects/first-website/scripts/main.js
+++ web-projects/first-website/scripts/main.js
@@ -18,7 +18,7 @@ function setUserName() {
     setUserName()
   } else {
     localStorage.setItem('name', myName)
-    myHeading.textContent = `Mozilla is cool, ${myName}`
+    myHeading.textContent = `FarmData2 is cool, ${myName}`
   }
 }
 
