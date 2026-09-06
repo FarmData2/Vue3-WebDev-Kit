3480a80845ec75c52bfba69b61fda40942dfe3c0
Grant Braught <braught@dickinson.edu>
2026-09-06 00:31:20 +0000

    moves flashword(v1) content into flasword-vite(v2)

--- web-projects/flashword-vite/src/App.vue
+++ web-projects/flashword-vite/src/App.vue
@@ -1,45 +1,154 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue';
+<script>
+export default {
+  data() {
+    return {
+      words: [
+        {
+          word_a: 'hola',
+          word_b: 'hello',
+          hint: 'greeting',
+          answer: '',
+          correct: false,
+        },
+        {
+          word_a: 'uno',
+          word_b: 'one',
+          hint: 'number',
+          answer: '',
+          correct: false,
+        },
+        {
+          word_a: 'gris',
+          word_b: 'grey',
+          hint: 'color',
+          answer: '',
+          correct: false,
+        },
+      ],
+      correctCount: 0,
+      completed: false,
+    };
+  },
+  computed: {
+    shuffledWords() {
+      return [...this.words].sort(() => 0.5 - Math.random());
+    },
+    wordCount() {
+      return this.words.length;
+    },
+  },
+  watch: {
+    correctCount() {
+      this.completed = this.correctCount == this.wordCount;
+    },
+  },
+  methods: {
+    checkAnswer(word) {
+      word.correct = word.word_b == word.answer;
+
+      if (word.correct) {
+        this.correctCount++;
+      }
+    },
+  },
+};
 </script>
 
 <template>
-  <div>
-    <a
-      href="https://vitejs.dev"
-      target="_blank"
-    >
-      <img
-        src="/vite.svg"
-        class="logo"
-        alt="Vite logo"
+  <div id="app" v-cloak>
+    <h1>FlashWord</h1>
+
+    <p v-if="completed" id="completed">
+      Good work, you have completed all the words!
+    </p>
+    <p v-else id="correctCount">
+      You have answered {{ correctCount }} out of {{ wordCount }}
+    </p>
+
+    <div id="cards">
+      <div
+        class="card"
+        v-for="word in shuffledWords"
+        v-bind:class="{ correct: word.correct }"
       >
-    </a>
-    <a
-      href="https://vuejs.org/"
-      target="_blank"
-    >
-      <img
-        src="./assets/vue.svg"
-        class="logo vue"
-        alt="Vue logo"
-      >
-    </a>
+        <p class="word">{{ word.word_a }}</p>
+        <input
+          type="text"
+          v-if="!word.correct"
+          v-model="word.answer"
+          v-on:keyup.enter="checkAnswer(word)"
+        />
+        <p v-else class="correctAnswer">{{ word.answer }}</p>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+[v-cloak] {
+  display: none;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: black;
+  margin-top: 60px;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+#cards {
+  justify-content: center;
+  display: grid;
+  grid-template-columns: 300px 300px 300px;
+  grid-gap: 30px;
+}
+
+.card {
+  background-color: #e8f0ff;
+  border-radius: 5px;
+  padding: 10px 0;
+  font-size: 25px;
+}
+
+input[type='text'] {
+  border: 0;
+  font-size: 25px;
+  border-radius: 5px;
+  margin-top: 5px;
+  text-align: center;
+  padding: 5px;
+}
+
+.word {
+  font-weight: bold;
+  padding: 0;
+  margin: 0;
+}
+
+.correctAnswer {
+  padding: 0;
+  margin: 0;
+}
+
+.correct {
+  color: #0f5132;
+  background-color: #d1e7dd;
+}
+
+#correctCount {
+  font-size: 20px;
+  margin: 10px;
+  font-weight: bold;
+  padding: 10px;
+}
+
+#completed {
+  font-size: 20px;
+  font-weight: bold;
+  color: #0f5132;
+  padding: 10px;
+  margin: 10px;
 }
 </style>
