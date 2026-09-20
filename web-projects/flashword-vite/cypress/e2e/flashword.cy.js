ceb420c97bacc61ed92ee3c309d9b84707e56f69
Grant Braught <braught@dickinson.edu>
2026-09-20 16:47:48 +0000

    adds check for 0 out of 3 message

--- web-projects/flashword-vite/cypress/e2e/flashword.cy.js
+++ web-projects/flashword-vite/cypress/e2e/flashword.cy.js
@@ -4,5 +4,8 @@ describe('FlashWord Tests', () => {
 
     // Get the <h1> element and check its contents.
     cy.get('h1').should('contain', 'FlashWord');
+
+    // Get the <p> element and check its contents.
+    cy.get('p').should('contain', 'You have answered 0 out of 3');
   });
 });
