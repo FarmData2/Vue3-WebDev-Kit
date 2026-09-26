5ed0cd0deafc91e22afbb4ef747843aaa0897a7e
Grant Braught <braught@dickinson.edu>
2026-09-26 12:20:06 +0000

    adds the assertion for 1 word correct

--- web-projects/flashword-vite/cypress/e2e/flashword.cy.js
+++ web-projects/flashword-vite/cypress/e2e/flashword.cy.js
@@ -76,5 +76,7 @@ describe('FlashWord Tests', () => {
       .should('have.class', 'correct')
       .find('.correctAnswer')
       .should('have.text', 'hello');
+
+    cy.get('[data-cy="num-correct"]').should('have.text', '1');
   });
 });
